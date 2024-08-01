045af5fc03610faa712532dfad5ae707fa6a9d8e
EHYONGI <irene4618@naver.com>
2024-08-01 11:39:03 +0900

    index/detail/create/migration(2)

--- articles/models.py
+++ articles/models.py
@@ -5,5 +5,6 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
 
-# class Comment(models.Model):
-#     pass
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE) # 부모가 지워지면 자식 요소도 지워주세요 
